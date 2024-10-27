09a2dde4b972ff976ca75f0c8a7c4283ab243bab
Trung78z <trungpspy@gmail.com>
2024-10-27 16:39:56 +0700

    new project

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import { createServer } from "http";
+import app from "./app";
+
+const PORT = process.env.PORT || 8080;
+
+const server = createServer(app);
+server.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
