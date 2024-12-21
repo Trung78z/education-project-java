f0e7a59d039a57a5d980edc4406c1f7a3b360b76
Trung78z <trungpspy@gmail.com>
2024-12-21 14:22:53 +0700

    update-backend

--- backend/src/main/java/com/api/backend/configs/WebConfig.java
+++ backend/src/main/java/com/api/backend/configs/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:4173", "http://localhost:5173")
+                .allowedOrigins("http://localhost:3000", "http://localhost:4173", "http://localhost:5173",
+                        "https://api.hcmuss.site", "https://car.hcmuss.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
