c3475ed8d4482f1c99941eb8819cf2fd25767a39
Trung78z <trungpspy@gmail.com>
2025-01-04 22:34:58 +0700

    remove file folder server

--- server/src/main/java/com/app/server/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.server.repository;
-
-import com.app.server.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
